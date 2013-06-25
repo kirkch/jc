3f1ac0a97affeeec1b185cbcc5ad63f3e0ba6521
ck <ck@zeebox.com>
2013-06-25 06:40:03 +0100

    implemented RepositoryRefParser

--- src/main/java/com/mosaic/jk/config/RepositoryRefParser.java
+++ src/main/java/com/mosaic/jk/config/RepositoryRefParser.java
@@ -6,7 +6,34 @@ package com.mosaic.jk.config;
 public class RepositoryRefParser {
 
     public RepositoryRef parseRef( String ref ) {
-        return null;
+        String[] tokens = splitRef(ref);
+
+        return new RepositoryRef(tokens[0], tokens[1]);
+    }
+
+    private String[] splitRef(String ref) {
+        String[] tokens = new String[2];
+
+        int index = getNameUrlSeparatorIndex(ref);
+        if ( index < 0 ) {
+            tokens[1] = ref.trim();
+        } else {
+            tokens[0] = ref.substring(0,index).trim();
+            tokens[1] = ref.substring(index+1).trim();
+        }
+
+        return tokens;
+    }
+
+    private int getNameUrlSeparatorIndex(String ref) {
+        int i = ref.indexOf(':');
+        if ( i < 0 ) {
+            return i;
+        } else if ( ref.length() > (i+1) && ref.charAt(i+1) != '/' ) {
+            return i;
+        }
+
+        return -1;
     }
 
 }
--- src/test/java/com/mosaic/jk/config/RepositoryRefParserTest.java
+++ src/test/java/com/mosaic/jk/config/RepositoryRefParserTest.java
@@ -16,12 +16,34 @@ public class RepositoryRefParserTest {
         String url = "http://myrepo.mycompany.com/repository";
         RepositoryRef ref = parser.parseRef(url);
 
-        assertEquals( url, ref.url );
+        assertEquals( url, ref.getUrl() );
     }
 
+    @Test
+    public void urlOnlyWithWhitespace() {
+        String url = "   http://myrepo.mycompany.com/repository \t  ";
+        RepositoryRef ref = parser.parseRef(url);
 
-    // urlAndRepositoryName
-    //invalidURL_expectException
+        assertEquals( "http://myrepo.mycompany.com/repository", ref.getUrl() );
+    }
+
+    @Test
+    public void urlAndRepositoryName() {
+        String url = " My Repo :  http://myrepo.mycompany.com/repository ";
+        RepositoryRef ref = parser.parseRef(url);
+
+        assertEquals( "My Repo", ref.getName() );
+        assertEquals( "http://myrepo.mycompany.com/repository", ref.getUrl() );
+    }
+
+//    @Test  -- decided not to perform the URL check here for now (will revisit)
+    public void invalidURL_expectException() {
+        String url = "http://myrepo. mycompany.com/repository";
+        RepositoryRef ref = parser.parseRef(url);
+
+        assertEquals( url, ref.getUrl() );
+    }
+
 
 
 }
